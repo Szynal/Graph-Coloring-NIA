dc9116b9ab82c95b49d6a0410fc3c45f3168e2ed
Paweł Szynal <pawel@szynal.info>
2021-11-29 01:00:17 +0100

    [GUI] Create error_message.py

---
+++ src/gui/error_message.py
@@ -0,0 +1,13 @@
+from PyQt5.QtWidgets import QMessageBox
+
+
+class GUIShowErrorMsg(object):
+
+    @staticmethod
+    def show_error_msg(text="Error", info_text="inf"):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Critical)
+        msg.setText("Error")
+        msg.setInformativeText(info_text)
+        msg.setWindowTitle("Error")
+        msg.exec_()
